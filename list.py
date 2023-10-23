487f9ecd4fb91370e5218a1d5483338c7bb55d92
anjalipahade529 <148784369+anjalipahade529@users.noreply.github.com>
2023-10-23 22:33:03 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,9 +1,1 @@
-list=[]
-print(type(list))
-subject=["toc","coa","ds","os","cd"]
-print(subject.index("os"))
-name=["Rohit","Rahul","Rohan","Romi","Rajesh"]
-print(name.append("Riya"))
-print(name.insert(2,"Pop"))
-#if alist contain colous voilet red green blue yellow add one colour amber in the last location
-#add one color black between red and yellow 
+
